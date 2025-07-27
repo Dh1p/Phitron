13caa33deab36943b3c7d0e162362632e4cf9fbf
Dh1p <uwazed@gmail.com>
2025-07-27 19:35:05 +0600

    New

--- Assignment -Exam/Same_or_Not_II.cpp
+++ Assignment -Exam/Same_or_Not_II.cpp
@@ -8,11 +8,54 @@ using namespace std;
 class MyStack
 {
     public:
-    
-}
+    vector<int>st;
+
+    void push(int val)
+    {
+        st.push_back(val);
+    }
+
+    void pop()
+    {
+        st.pop_back();
+    }
+
+    bool empty()
+    {
+        return st.empty();
+    }
+};
+
+class MyQueue
+{
+    public;
+    list<int>l;
+
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+
+    void pop()
+    {
+        l.pop_front();
+    }
+
+    bool empty()
+    {
+        return l.empty();
+    }
+};
 
 int main()
 {
+    MyStack s;
+    MyQueue q;
+
+    int m,n;
+    cin >> m >> n;
+
     
+
     return 0;
 }
