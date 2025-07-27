bece6b5d00ad8ef2e8f8585df2b7ab25598a42fc
Dh1p <uwazed@gmail.com>
2025-07-27 19:37:58 +0600

    Exam qs2

--- Assignment -Exam/Same_or_Not_II.cpp
+++ Assignment -Exam/Same_or_Not_II.cpp
@@ -20,6 +20,11 @@ class MyStack
         st.pop_back();
     }
 
+    int top()
+    {
+        return st.back();
+    }
+
     bool empty()
     {
         return st.empty();
@@ -28,7 +33,7 @@ class MyStack
 
 class MyQueue
 {
-    public;
+    public:
     list<int>l;
 
     void push(int val)
@@ -39,6 +44,11 @@ class MyQueue
     void pop()
     {
         l.pop_front();
+    }
+
+    int front()
+    {
+        return l.front();
     }
 
     bool empty()
@@ -55,7 +65,46 @@ int main()
     int m,n;
     cin >> m >> n;
 
-    
+    if(m!=n)
+    {
+        cout<< "NO" << endl;
+    }
+    else
+    {
+        for(int i=0; i<m; i++)
+        {
+            int val;
+            cin >> val;
+            s.push(val);
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            int val;
+            cin >> val;
+            q.push(val);
+        }
+        int fl = 0;
+        while(!s.empty() && !q.empty())
+        {
+            if(s.top()!=q.front())
+            {
+                cout << "NO" << endl;
+                fl = 1;
+                break;
+            }
+            else
+            {
+                s.pop();
+                q.pop();
+            }
+        }
+
+        if(fl == 0)
+        {
+            cout << "YES" << endl;
+        }
+    }
 
     return 0;
 }
